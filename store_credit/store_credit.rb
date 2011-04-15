d84d9219d74662e0f73c82887da352b4abf43fc9
Daehyun Kim <hatemogi@gmail.com>
2011-04-15 15:46:04 +0900

    small style change

--- store_credit/store_credit.rb
+++ store_credit/store_credit.rb
@@ -1,21 +1,19 @@
-def _solve credit, items
+def solve credit, items
   head, *rest = items 
   if found = rest.find {|p| head[0] + p[0] == credit}
-    [head, found]
-  else
-    _solve credit, rest
-  end   
+    return [head, found]
+  end
+  solve credit, rest
 end
 
-def solve credit, items
+def eachset credit, items
   indexed = items.zip((1..items.length).to_a)
   cand = indexed.select {|p| p[0] < credit}.sort {|p| p[0]}.reverse 
-  values = _solve(credit, cand)
-  values.collect(&:last).sort
+  solve(credit, cand).collect(&:last).sort
 end
 
 gets.to_i.times do |no|
   c = gets.to_i
   i, items = gets.to_i, gets.split.collect(&:to_i)
-  puts "Case ##{no += 1}: #{solve(c, items).join(' ')}"  
+  puts "Case ##{no += 1}: #{eachset(c, items).join(' ')}"  
 end
