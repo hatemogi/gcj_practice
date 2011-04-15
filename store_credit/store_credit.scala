7f6123c83314f0ce13b5d8c79ecb6cd4af29e916
Daehyun Kim <hatemogi@gmail.com>
2011-04-15 15:49:48 +0900

    style up

--- store_credit/store_credit.scala
+++ store_credit/store_credit.scala
@@ -11,12 +11,11 @@ object StoreCredit extends Application {
 		solve(credit, items.slice(1, items.length))
 	}
 	
-	val N = readInt()
-	for (n <- List.range(1, N + 1)) {
+	for (n <- List.range(1, readInt() + 1)) {
 		val credit = readInt()
 		val count = readInt()
-		val values = readLine.split(" ").map((s: String) => s.toInt)
-		val items = values.toList.zip(List.range(1, count + 1))
+		val values = readLine.split(" ").map((s: String) => s.toInt).toList
+		val items = values.zip(List.range(1, count + 1))
 		val r = solve(credit, items)
 		val indice = List(r._1._2, r._2._2).sortWith((a, b) => a < b).mkString(" ")
 		println("Case #" + n + ": " + indice)		
