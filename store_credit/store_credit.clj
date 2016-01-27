530276c88b6cd0f4d0357d2381bc722116f86662
Daehyun Kim <hatemogi@gmail.com>
2016-01-28 02:55:00 +0900

    add comments and refactor little.

--- store_credit/store_credit.clj
+++ store_credit/store_credit.clj
@@ -1,10 +1,13 @@
+;; https://code.google.com/codejam/contest/dashboard?c=351101
+;; $ java -jar ./clojure-1.8.0.jar store_credit.clj < A-large-practice.in > A-large-clj.out
+
 (defn solve [idx credit prices]
   (let [head (first prices)
-        cand (rest prices)]
-    (let [another (.indexOf cand (- credit head))]
-      (if-not (neg? another)
-        [idx (+ idx 1 another)]
-        (recur (inc idx) credit cand)))))
+        cand (rest prices)
+        another (inc (.indexOf cand (- credit head)))]
+    (if (pos? another)
+      [idx (+ idx another)]
+      (recur (inc idx) credit cand))))
 
 (dotimes [n (read-string (read-line))]
   (let [[credit _ prices] (repeatedly 3 read-line)
