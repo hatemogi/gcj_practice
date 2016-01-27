61680f5e42019e2e7145f43e80fa39e0e60b301b
Daehyun Kim <hatemogi@gmail.com>
2016-01-28 01:29:28 +0900

    speed up. but readablity may suffer

--- store_credit/store_credit.clj
+++ store_credit/store_credit.clj
@@ -1,17 +1,13 @@
-(defn solve [credit indexed]
-  (let [head (first indexed)
-        cand (rest indexed)
-        matchp #(= credit (+ (last %) (last head)))]
-    (if-let [another (first (filter matchp cand))]
-      (sort (map first [head another]))
-      (recur credit cand))))
-
-(defn answer [credit prices]
-  (let [indexed (zipmap (iterate inc 1) prices)]
-    (solve credit indexed)))
+(defn solve [idx credit prices]
+  (let [head (first prices)
+        cand (rest prices)]
+    (let [another (.indexOf cand (- credit head))]
+      (if-not (neg? another)
+        [idx (+ idx 1 another)]
+        (recur (inc idx) credit cand)))))
 
 (dotimes [n (read-string (read-line))]
   (let [[credit _ prices] (repeatedly 3 read-line)
         prices (->> (re-seq #"\d+" prices) (map read-string))
-        a (answer (read-string credit) prices)]
+        a (solve 1 (read-string credit) prices)]
     (println (str "Case #" (inc n) ": " (first a) " " (last a)))))
