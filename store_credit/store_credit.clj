c6f2237a6ca7ff42eb52a735c1dc4710666e90dd
Daehyun Kim <hatemogi@gmail.com>
2016-01-28 00:30:14 +0900

    store_credit.clj shorter version

--- store_credit/store_credit.clj
+++ store_credit/store_credit.clj
@@ -1,24 +1,17 @@
 (defn solve [credit indexed]
   (let [head (first indexed)
         cand (rest indexed)
-        match #(= credit (+ (:val %) (:val head)))]
-    (if-let [another (first (filter match cand))]
-      (sort (map :idx [head another]))
+        matchp #(= credit (+ (last %) (last head)))]
+    (if-let [another (first (filter matchp cand))]
+      (sort (map first [head another]))
       (recur credit cand))))
 
-(defn answer [[credit _ prices]]
-  (let [credit (read-string credit)
-        prices (->> (re-seq #"\d+" prices)
-                    (map read-string))
-        indexed (map #(hash-map :idx %1 :val %2) (iterate inc 1) prices)]
+(defn answer [credit prices]
+  (let [indexed (zipmap (iterate inc 1) prices)]
     (solve credit indexed)))
 
-(defn format-answers [answers]
-  (map #(str "Case #" %1 ": " (first %2) " " (last %2))
-       (iterate inc 1)
-       answers))
-
-(let [lines (rest (line-seq (java.io.BufferedReader. *in*)))
-      problems (partition 3 lines)]
-  (doseq [answer (format-answers (map answer problems))]
-    (println answer)))
+(dotimes [n (read-string (read-line))]
+  (let [[credit _ prices] (repeatedly 3 read-line)
+        prices (->> (re-seq #"\d+" prices) (map read-string))
+        a (answer (read-string credit) prices)]
+    (println (str "Case #" (inc n) ": " (first a) " " (last a)))))
